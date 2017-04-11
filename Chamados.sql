9aa4ce9cc808a8d1a860abaae91ad47b49d5613b
Lucas Duarte <lucasdu4rte@gmail.com>
2017-04-11 11:07:58 -0300

    Inclusão do arquivo.SQL, Qry:Create Chamado

---
+++ Chamados.sql
@@ -0,0 +1,17 @@
+CREATE TABLE chamado (
+	id  not null AUTOINCREMENT,
+	responsavel integer,
+	solicitante not null integer,
+	data_inc date,
+	data_alt date,
+	data_prazo date,
+	descricao varchar(255),
+	observacao varchar (400),
+	id_status integer,
+	id_tipo integer,
+	PRIMARY KEY (id),
+	CONSTRAINT fk_id_status FOREIGN KEY (id_status) REFERENCES Status (id),
+	CONSTRAINT fk_id_tipo FOREIGN KEY (id_tipo) REFERENCES Tipo (id),
+	CONSTRAINT fk_id_solicitante FOREIGN KEY (solicitante) REFERENCES Usuario (id),
+	CONSTRAINT fk_id_responsavel FOREIGN KEY (responsavel) REFERENCES Usuario (id),
+)
