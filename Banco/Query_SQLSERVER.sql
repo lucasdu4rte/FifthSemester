5001af082fd86611791795f16f653e956b759f10
Lucas Duarte <lucasdu4rte@gmail.com>
2017-04-21 09:10:15 -0300

    Organização QUERY SQLSERVER

--- Banco/Query_SQLSERVER.sql
+++ Banco/Query_SQLSERVER.sql
@@ -1,53 +1,54 @@
 CREATE DATABASE ticket
+use ticket
+CREATE TABLE status_chamado (
+	id int PRIMARY KEY identity(1,1),
+	descricao varchar(200)
+);
 
-USE ticket
+CREATE TABLE tipo (
+	id int PRIMARY KEY identity(1,1),
+	descricao varchar(200)
+);
+
+CREATE TABLE departamento (
+	id int PRIMARY KEY identity(1,1),
+	descricao varchar(250) not null
+);
+
+CREATE TABLE funcionario(
+	id int PRIMARY KEY identity(1,1),
+	nome varchar(100),
+	data_nasc varchar(50),
+	email varchar(50),
+	telefone varchar(50),
+	celular varchar(50),
+	ramal varchar(50),
+	id_departamento int,
+	CONSTRAINT fk_id_departamento FOREIGN KEY (id_departamento) REFERENCES departamento(id)
+);
+
 
 CREATE TABLE chamado (
-	id int identity(1,1),
-	responsavel integer,
-	solicitante int not null,
+	id int PRIMARY KEY identity(1,1),
+	id_responsavel int,
+	id_solicitante int not null,
 	data_inc date,
 	data_alt date,
 	data_prazo date,
-	descricao varchar(250),
-	observacao varchar (400),
-	id_status integer,
-	id_tipo integer,
-	PRIMARY KEY (id),
-	CONSTRAINT fk_id_status FOREIGN KEY (id_status) REFERENCES Status (id),
+	descricao varchar(255),
+	observacao varchar(400),
+	id_status int,
+	id_tipo int,
+	CONSTRAINT fk_id_status_chamado FOREIGN KEY (id_status) REFERENCES status_chamado (id),
 	CONSTRAINT fk_id_tipo FOREIGN KEY (id_tipo) REFERENCES Tipo (id),
-	CONSTRAINT fk_id_solicitante FOREIGN KEY (solicitante) REFERENCES funcionario (id),
-	CONSTRAINT fk_id_responsavel FOREIGN KEY (responsavel) REFERENCES funcionario (id),
-)
+	CONSTRAINT fk_id_solicitante FOREIGN KEY (id_solicitante) REFERENCES funcionario(id),
+	CONSTRAINT fk_id_responsavel FOREIGN KEY (id_responsavel) REFERENCES funcionario(id)
+);
 
-CREATE TABLE status(
-id int,
-descricao varchar(200),
-primary key(id)
-)
-
-CREATE TABLE tipo(
-id int,
-descricao varchar(200),
-primary key(id)	
-)
-
-CREATE TABLE funcionario(
-id int identity,
-nome varchar(100),
-data_nasc varchar(50),
-salario float,
-email varchar(50),
-telefone varchar(50),
-celular varchar(50),
-ramal varchar(50),
-id_departamento int,
-primary key(id),
-CONSTRAINT fk_id_departamento FOREIGN KEY (id_departamento) REFERENCES departamento (id)
-)
-
-CREATE TABLE departamento (
-	id int not null identity,
-	Descricao  varchar(250)not null,
-	PRIMARY KEY(id)
+CREATE TABLE funcionarioxchamado (
+	id int PRIMARY KEY identity(1,1),
+	id_funcionario int,
+	id_chamado int,
+	CONSTRAINT fk_id_funcionario FOREIGN KEY (id_funcionario) REFERENCES funcionario(id),
+	CONSTRAINT fk_id_chamado FOREIGN KEY (id_chamado) REFERENCES chamado (id)
 )
