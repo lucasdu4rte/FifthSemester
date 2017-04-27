2a5513eb842a5fa8784108c608f72159ee8e3e75
Lucas Duarte <lucasdu4rte@gmail.com>
2017-04-26 22:19:57 -0300

    Correção de bugs (Conexao.php, recebe_func.php)

--- WebTicket/cad_func.php
+++ WebTicket/cad_func.php
@@ -1,65 +1,64 @@
-	<?php
-		include_once('includes/cabecalho.php');
-	?>
+<?php
+        include_once('includes/cabecalho.php');
+?>
+
+<div class="container">
+    <form name="cad_func" method="post" action="recebe_func.php">
+        <h1>Cadastro de Funcionários</h1><br/>
+        <div class="row">
+            <div class="form group col-md-4">
+                <label>Nome</label>
+                <input type="text" name="nome_func" id="nome_func" maxlength="40" class="form-control" placeholder="Digite o seu nome..." />	
+            </div>
+            <div class="form group col-md-4">
+                <label>Data de Nascimento</label>
+                <input type="text" name="nasc_func" id="nasc_func" maxlength="40" class="form-control" placeholder="00/00/0000" />	
+            </div>
+        </div>
+        <div class="row">
+            <div class="form group col-md-4">
+                <label>Telefone</label>
+                <input type="text" name="tel_func" id="tel_func" maxlength="40" class="form-control" placeholder="(00) 0000-0000" />	
+            </div>
+            <div class="form group col-md-4">
+                <label>Celular</label>
+                <input type="text" name="cel_func" id="cel_func" maxlength="40" class="form-control" placeholder="(00) 0000-0000" />	
+            </div>		
+        </div>
+        <div class="row">
+            <div class="form group col-md-4">
+                <label>Ramal</label>
+                <input type="text" name="ramal_func" id="ramal_func" maxlength="40" class="form-control" placeholder="Nº do Ramal" />	
+            </div>
+        </div>
+        <div class="form group col-md-4">
+            <label>Departamento</label>
+            <input type="text" name="dpto_func" id="dpto_func" maxlength="40" class="form-control" placeholder="(00) 0000-0000" />	
+        </div>
+        <div class="row">
+            <div class="form group col-md-8">
+                <label>Email</label>
+                <input type="email" name="email_func" id="email_func" maxlength="40" class="form-control" placeholder="Digite o seu endereço de Email..."/>
+            </div>
+        </div><br/>
+        <input type="hidden" name="enviou" value="True" />
+        <div id="actions" align="left">
+            <a href="#"> 
+                <input type="reset" class="btn btn-default" value="Cancelar"/>
+            </a>
+            <input type="submit"class="btn btn-primary submit" value="Cadastrar"/>
+        </div>			
+    </form>
+</div> 
+<!-- Jquery Mask -->
+<script type="text/javascript">
+    $(function() {              
+        $('#nasc_func').mask("99/99/9999");
+        $("#cel_func").mask("(99) 99999-9999");
+                        $("#tel_func").mask("(99) 9999-9999");			
+    });	
+</script>
 	
-	<div class="container">
-		<form name="cad_func" method="post" action="recebe_func.php">
-			<h1>Cadastro de Funcionários</h1><br/>
-			<div class="row">
-				<div class="form group col-md-4">
-					<label>Nome</label>
-                    <input type="text" name="nome_func" id="nome_func" maxlength="40" class="form-control" placeholder="Digite o seu nome..." />	
-				</div>
-				<div class="form group col-md-4">
-					<label>Data de Nascimento</label>
-                    <input type="text" name="nasc_func" id="nasc_func" maxlength="40" class="form-control" placeholder="00/00/0000" />	
-				</div>
-			</div>
-			
-			<div class="row">
-				<div class="form group col-md-4">
-					<label>Telefone</label>
-                    <input type="text" name="tel_func" id="tel_func" maxlength="40" class="form-control" placeholder="(00) 0000-0000" />	
-				</div>
-				<div class="form group col-md-4">
-					<label>Celular</label>
-                    <input type="text" name="cel_func" id="cel_func" maxlength="40" class="form-control" placeholder="(00) 0000-0000" />	
-				</div>		
-			</div>
-			<div class="row">
-				<div class="form group col-md-4">
-					<label>Ramal</label>
-                    <input type="text" name="ramal_func" id="ramal_func" maxlength="40" class="form-control" placeholder="Nº do Ramal" />	
-				</div>
-				<div class="form group col-md-4">
-					<label>Departamento</label>
-                    <input type="text" name="dpto_func" id="dpto_func" maxlength="40" class="form-control" placeholder="(00) 0000-0000" />	
-				</div>
-			</div>
-			<div class="row">
-				<div class="form group col-md-8">
-					<label>Email</label>
-					<input type="email" name="email_func" id="email_func" maxlength="40" class="form-control" placeholder="Digite o seu endereço de Email..."/>
-				</div>
-			</div><br/>
-			<input type="hidden" name="enviou" value="True" />
-			<div id="actions" align="left">
-                    <a href="#"> 
-                        <input type="reset" class="btn btn-default" value="Cancelar"/>
-                    </a>
-                   <input type="submit"class="btn btn-primary submit" value="Cadastrar"/>
-            </div>			
-		</form>
-	</div> 
-	<!-- Jquery Mask -->
-	<script type="text/javascript">
-            $(function() {              
-                $('#nasc_func').mask("99/99/9999");
-                $("#cel_func").mask("(99) 99999-9999");
-				$("#tel_func").mask("(99) 9999-9999");			
-            });	
-    </script>
-	
-	<?php
-		include_once('includes/rodape.php');
-	?>
+<?php
+include_once('includes/rodape.php');
+?>
--- WebTicket/includes/conexao.php
+++ WebTicket/includes/conexao.php
@@ -1,16 +1,14 @@
-<?
+<?php
+
   //Define as informações de acesso
   //ao banco de dacos como constantes
-  echo 'teste';
-  define('DB_SERVIDOR','localhost'); 
-  define('DB_USUARIO','root');
-  define('DB_SENHA','');
-  define('DB_BANCO','webticket');
+  define('DB_SERVIDOR', 'localhost'); 
+  define('DB_USUARIO', 'root');
+  define('DB_SENHA', '');
+  define('DB_BANCO', 'ticket');
   
-  $dbc = @mysqli_connect(DB_SERVIDOR,DB_USUARIO,DB_SENHA,DB_BANCO);
+  $dbc = @mysqli_connect(DB_SERVIDOR,DB_USUARIO,DB_SENHA,DB_BANCO) or die();
 
 if (!$dbc)
   trigger_error('Não foi possível conectar ao MySQL: ' . mysqli_connect_error());
 
-echo 'teste';
-?>
